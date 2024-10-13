d1911fc3ba408fbebbb73ceccd85ad44d8448d37
WillianSeidel <willianrobertoseidel@gmail.com>
2024-10-13 11:27:07 -0300

    feat: conversao

---
+++ conversao.py
@@ -0,0 +1,31 @@
+#conversão de tipos string para int etc...
+
+#inteiro para float
+preco = 10
+print(preco)
+
+preco = float(preco)
+print(preco)
+
+#float para inteiro
+preco = float(10.30)
+print(preco)
+
+preco = int(preco)
+print(preco)
+
+#numero para string
+s_preco = 5.0
+print(str(preco))
+
+texto = f'O preço é {preco} mas converti em string {s_preco} valeu'
+print(texto)
+
+#string para numero
+preco = "10.50"
+idade = "28"
+
+print(float(preco))
+print(int(idade))
+
+#não consegue converter streing para float quando não existir numero
---
+++ inicio.py
@@ -0,0 +1,19 @@
+#age referencia uma variavel 
+age = 28
+#name referencia uma string nome
+name = 'Willian'
+print(f'Meu nome é {name} e tenho {age} anos')
+
+#constantes não mudam, não alteram o valor, em python não é possivel declarar constantes, entretanto a convenção determina LETRAS MAIUSCULAS
+
+AGE = 28
+NAME = 'JULIA'
+print (f'Oi meu nome é {NAME} e tenho {AGE}')
+
+# SNAKE CASE nomes com espaços sempre utiliza o underline
+EXEMPLO_01 = 'Foi usado o underline'
+limite_diario = 1000
+print(EXEMPLO_01, limite_diario)
+
+
+
