45390444de26f03f133d0fbbe89a73ce2e9d4f93
WillianSeidel <willianrobertoseidel@gmail.com>
2024-10-19 10:27:50 -0300

    feat: tuplas

---
+++ tupla.py
@@ -0,0 +1,8 @@
+#estrutura imutavel 
+#classe é tuple usado como construtor ou pode ser colocado entre parenteses
+
+#exemplo
+frutas = ("laranja", "pera", "uva")
+letras = tuple("python")
+numeros = tuple([1,2,3,4])
+pais = ("Brasil",)
