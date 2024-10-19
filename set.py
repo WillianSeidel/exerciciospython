b3e2f730f295731e7078e2c2c1d8c25bd66d4130
WillianSeidel <willianrobertoseidel@gmail.com>
2024-10-19 11:01:28 -0300

    feat: set

---
+++ set.py
@@ -0,0 +1,11 @@
+set([1,2,3,4,5,4,5,4]) # {1,2,3,4,5}
+set("abacaxi") #{"b","a","c","x","i"}
+
+#{}.intersection
+conjunto_a = {1,2,3}
+conjunto_b = {2,3,4}
+
+conjunto_a.intersection(conjunto_b)
+
+#{}.add
+#{}.clear
