f97febbe92295af2ebcbd0ce93b7d05e384f9a69
WillianSeidel <willianrobertoseidel@gmail.com>
2024-10-20 11:50:28 -0300

    feat: dicionario

---
+++ dicionario.py
@@ -0,0 +1,26 @@
+pessoa = {"nome": "willian", "idade": 28}
+pessoas = dict(nome="willian", idade=29)
+
+#acesso ao valor
+pessoa["nome"]
+
+#sobrescrever o valor 
+pessoas["nome"] ="maria"
+#adicionar chaves
+dict.fromkeys
+#forma de acessar
+{}.get()
+#retornar lista
+{}.itens()
+#retorna novo dicionario
+{}.keys()
+#pop remove uma chave
+{}.pop()
+#atualizar
+{}.update()
+#retornar os valores
+{}.values()
+#metodo in
+contatos = "Roberto"
+resultado = "willian" in contatos
+print(resultado)
