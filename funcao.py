f7fa90921db27f48f137137025d7750637fbb09e
WillianSeidel <willianrobertoseidel@gmail.com>
2024-10-24 20:15:21 -0300

    feat: func

---
+++ funcao.py
@@ -0,0 +1,15 @@
+# funções 
+#é um bloco de código indetificado que recebe parametros, pode ser valores ou não, possibilita reapoveitamento
+def exibir_mensagem():
+    print("teste 01")
+def exibir(nome = "willian"):
+    print(f"seja bem vindo {nome}")
+
+exibir()
+
+def soma():
+    numero1 = 4
+    numero2 = 2
+    numero = (numero1 * numero2)
+    print(numero)
+soma()
